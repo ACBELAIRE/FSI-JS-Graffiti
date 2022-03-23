76fd0eeb8be1de9e3d975f3f6c463cc4b611c0a6
Your Name <you@example.com>
2022-03-22 18:59:17 -0700

    dog image border'

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 let header = document.querySelector('#page-header')
     header.style.textAlign = 'left'
 // Select some elements...
+
+let dogimages = document.querySelectorAll('.dog-image')
+...
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.borderRadius = '50px'
+}
