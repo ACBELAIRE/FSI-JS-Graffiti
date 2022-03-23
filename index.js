12bae21b42a50a3904929dbccf98d92b8b83d04b
Your Name <you@example.com>
2022-03-22 19:09:49 -0700

    modify style of multiple elements

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let header = document.querySelector('#page-header')
     header.style.textAlign = 'left'
-// Select some elements...
 
 let dogimages = document.querySelectorAll('.dog-image')
 for(let i = 0; i < dogImages.length; i++){
