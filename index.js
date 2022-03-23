1f61d9cec40e9008932821988165c9fb95803d85
Your Name <you@example.com>
2022-03-23 12:04:57 -0700

    select multiple elements.. dogImages

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+//Select and Style Element
+
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
+
+//Select Multiple Elements
+let dogImages = document.querySelectorAll('.dog-image')
