89125345cc64527a2e6b70dbec5e3d010134a1ed
Your Name <you@example.com>
2022-03-22 19:12:31 -0700

    modify border

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let header = document.querySelector('#page-header')
     header.style.textAlign = 'left'
 
-let dogimages = document.querySelectorAll('.dog-image')
-for(let i = 0; i < dogImages.length; i++){
-	dogImages[i].style.borderRadius = '50px'
-}
+    ...
+    let dogImages = document.querySelectorAll('.dog-image')
+    for(let i = 0; i < dogImages.length; i++){
+        dogImages[i].style.borderRadius = '50px'
+    }
