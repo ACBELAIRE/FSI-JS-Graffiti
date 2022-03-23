56526089e15b76b8e10501525e24cb10ad867031
Your Name <you@example.com>
2022-03-22 19:21:07 -0700

    dog name align left

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let header = document.querySelector('#page-header')
     header.style.textAlign = 'left'
 
-    ...
+
     let dogImages = document.querySelectorAll('.dog-image')
     for(let i = 0; i < dogImages.length; i++){
         dogImages[i].style.borderRadius = '50px'
     }
-    
+
+    let "dog-name" = document.querySelector('#dog-name')
+    "dog-name".style.textAlign = 'left'
+
